3120ce976270feabf413c9a75c5e9d2306f742e5
Alain Bonneau <alaindev97@gmail.com>
2026-01-20 11:30:42 +0100

    feat: add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,33 @@
-import "./page.scss";
+import Image from 'next/image';
+import Link from 'next/link';
+import './page.scss';
 
 export default function Login() {
   return (
     <div className="login-page-container">
-      <h1>Connexion</h1>
+      <div className="left-container">
+        <Image src="/images/abricot.png" alt="Logo Abricot" width={252} height={32} />
+        <div className="form-container">
+          <h1>Connexion</h1>
+          <form>
+            <div className="form-field">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className="form-field">
+              <label htmlFor="password">Mot de passe</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <button type="submit">Se connecter</button>
+            <Link href="/forgot-password">Mot de passe oublié ?</Link>
+          </form>
+        </div>
+        <div className="create-account">
+          <p>Pas encore de compte ?</p>
+          <Link href="/register">Créer un compte</Link>
+        </div>
+      </div>
+      <div className="right-container"></div>
     </div>
   );
 }
