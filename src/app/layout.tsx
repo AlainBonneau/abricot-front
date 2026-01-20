a209a010873e69dd6d2030300a0fe2b3ef951314
Alain Bonneau <alaindev97@gmail.com>
2026-01-20 08:50:18 +0100

    chore: add variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manrope = Manrope({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-manrope",
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${manrope.variable}`}>{children}</body>
     </html>
   );
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import "./page.scss";
+
+export default function Login() {
+  return (
+    <div className="login-page-container">
+      <h1>Connexion</h1>
+    </div>
+  );
+}
