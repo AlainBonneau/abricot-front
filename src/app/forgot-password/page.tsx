7ff4140c60071925730ddc4f444dc1ac9cddb8d6
Alain Bonneau <alaindev97@gmail.com>
2026-03-17 23:11:23 +0100

    feat/ add forgot password page

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import './page.scss';
+
+export default function ForgotPasswordPage() {
+  return (
+    <div className="forgot-password-page-container">
+      <h1 className="forgot-page-title">Fonctionnalité à venir</h1>
+    </div>
+  );
+}
